0b941577f8bf574bfe7353e4f1abaf2c633be6c7
Olegkirkicha <olegkirkicha@gmail.com>
2025-04-04 00:02:28 +0300

    styles improved with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],  // Тут має бути правильний шлях до HTML файлів
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
